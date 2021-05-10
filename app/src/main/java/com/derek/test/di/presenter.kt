1aa4b098ddefdf319a7182e55b88579d6a3b725a
a984983 <57126508+a984983@users.noreply.github.com>
2021-05-10 23:05:41 +0800

    add presenter test

--- app/src/main/java/com/derek/test/di/presenter.kt
+++ app/src/main/java/com/derek/test/di/presenter.kt
@@ -7,14 +7,10 @@ import com.derek.test.presenter.main.UserListPresenter
 import com.derek.test.presenter.userdetails.UserDetailsPresenter
 import com.derek.test.repository.userdetail.UserDetailsRepositoryImpl
 import com.derek.test.repository.userlist.UserListRepositoryImpl
-import com.derek.test.usecase.userlist.UserListUseCase
-import com.derek.test.usecase.userlist.UserListUseCaseImpl
 import com.derek.test.view.userdetails.IUserDetailsView
 import org.koin.dsl.module
 
 val presenterModule = module {
-
-    factory<UserListUseCase> { UserListUseCaseImpl(UserListRepositoryImpl(get())) }
 
     factory { (view: IMainView) ->
         UserListPresenter(view, UserListInteractor(UserListRepositoryImpl(get())))
